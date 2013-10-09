cd538f32f97ce786d8d7ba9b28b9cf04ea114a12
b2un0 <branko.wilhelm@gmail.com>
2013-10-09 09:02:23 +0200

    first public release

---
+++ fail2ban.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author     mediahof, Kiel-Germany
+ * @link       http://www.mediahof.de
+ * @copyright  Copyright (C) 2013 mediahof. All rights reserved.
+ * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+defined('_JEXEC') or die;
+
+class plgSystemFail2Ban extends JPlugin {
+	
+	public function onUserLoginFailure(array $response){
+		error_log(sprintf($this->params->get('message'), $response['username']));
+	}
+}
